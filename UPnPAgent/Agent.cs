e28d0979633df368a09b1ec479ae48d5deb6c34b
Brian <brian.bacon01@gmail.com>
2018-01-15 21:46:22 -0500

    updated agent

--- UPnPAgent/Agent.cs
+++ UPnPAgent/Agent.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
@@ -30,26 +31,40 @@ namespace UPnPAgent
 
         public async Task Run()
         {
+            int.TryParse(ConfigurationManager.AppSettings["DelaySeconds"], out int delay);
+
             IPAddress externalIp = IPAddress.None;
+            var discoverer = new NatDiscoverer();
+            var device = await discoverer.DiscoverDeviceAsync();
 
             while (true)
             {
-                var discoverer = new NatDiscoverer();
-                var device = await discoverer.DiscoverDeviceAsync();
                 var ip = await device.GetExternalIPAsync();
 
                 if (externalIp != ip)
                 {
                     externalIp = ip;
-                    var mapping = await device.GetSpecificMappingAsync(Protocol.Tcp, 88);
-                    if (mapping == null)
+                    foreach (var portConfig in ConfigurationManager.AppSettings["Ports"].Split(new char[] { ',' }))
                     {
-                        mapping = new Mapping(Protocol.Tcp, 8180, 88);
-                        device.CreatePortMapAsync(mapping).Wait();
+                        var info = portConfig.Split(new char[] { ':' });
+
+                        Protocol protocol = (Protocol)Enum.Parse(typeof(Protocol), info[0]);
+                        int port = int.Parse(info[1]);
+
+                        try
+                        {
+                            var mapping = await device.GetSpecificMappingAsync(protocol, port);
+                            if (mapping == null)
+                            {
+                                mapping = new Mapping(protocol, port, port);
+                                await device.CreatePortMapAsync(mapping);
+                            }
+                        }
+                        catch { }
                     }
                 }
 
-                await Task.Delay(TimeSpan.FromHours(1));
+                await Task.Delay(TimeSpan.FromMinutes(delay));
             }
         }
     }
