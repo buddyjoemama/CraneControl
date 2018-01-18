f05c9ab0f5d90962e845a9ee5f58dd20b430c9c8
Brian <brian.bacon01@gmail.com>
2018-01-17 21:14:05 -0500

    updated service

--- UPnPAgent/Agent.Designer.cs
+++ UPnPAgent/Agent.Designer.cs
@@ -28,8 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            // 
+            // Agent
+            // 
             this.ServiceName = "Service1";
+
         }
 
         #endregion
