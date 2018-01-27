7ebb580544056697bf3ad46a17b947b5a3978ec9
Brian <brian.bacon01@gmail.com>
2018-01-26 22:20:33 -0500

    Added web project

--- Firmware/SerialReader/SerialReader/SerialReader/Sketch.cpp
+++ Firmware/SerialReader/SerialReader/SerialReader/Sketch.cpp
@@ -17,58 +17,58 @@ const int clockPin = 11;
 const int dataPin = 12;
 
 void setup() {
-  Serial.begin(9600);
+	Serial.begin(9600);
 
-  pinMode(latchPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
+	pinMode(latchPin, OUTPUT);
+	pinMode(clockPin, OUTPUT);
+	pinMode(dataPin, OUTPUT);
 
-  // POST
-  digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, MSBFIRST, 0);
-  shiftOut(dataPin, clockPin, MSBFIRST, 0);
-  digitalWrite(latchPin, HIGH);  
-  
-  int postVal = analogRead(0);
-  if(postVal <= 10) 
-  {
-    for(int i = 0; i < 6; i++) 
-    {
-      Shift(1 << i, 0);
-      delay(500);
-    }
+	// POST
+	digitalWrite(latchPin, LOW);
+	shiftOut(dataPin, clockPin, MSBFIRST, 0);
+	shiftOut(dataPin, clockPin, MSBFIRST, 0);
+	digitalWrite(latchPin, HIGH);
+	
+	int postVal = analogRead(0);
+	if(postVal <= 10)
+	{
+		for(int i = 0; i < 6; i++)
+		{
+			Shift(1 << i, 0);
+			delay(500);
+		}
 
-    // Reset
-    Shift(0, 0);
+		// Reset
+		Shift(0, 0);
 
-    for(int i = 0; i < 6; i++) 
-    {
-      Shift(0, 1 << i);
-      delay(500);
-    }
-    
-    Shift(255, 255);
-    delay(1000);
-  }
-  
-  Shift(0, 0);
+		for(int i = 0; i < 6; i++)
+		{
+			Shift(0, 1 << i);
+			delay(500);
+		}
+		
+		Shift(255, 255);
+		delay(1000);
+	}
+	
+	Shift(0, 0);
 }
 
-void loop() 
+void loop()
 {
-  char buffer[2];
-  
-  if(Serial.available() > 0) {
-    if(Serial.readBytes(buffer, 2) > 0) {
-      Shift(buffer[0], buffer[1]);
-    }
-  }
+	char buffer[2];
+	
+	if(Serial.available() > 0) {
+		if(Serial.readBytes(buffer, 2) > 0) {
+			Shift(buffer[0], buffer[1]);
+		}
+	}
 }
 
 void Shift(int northChip, int southChip)
 {
-  digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, MSBFIRST, southChip);
-    shiftOut(dataPin, clockPin, MSBFIRST, northChip);
-  digitalWrite(latchPin, HIGH);    
+	digitalWrite(latchPin, LOW);
+	shiftOut(dataPin, clockPin, MSBFIRST, southChip);
+	shiftOut(dataPin, clockPin, MSBFIRST, northChip);
+	digitalWrite(latchPin, HIGH);
 }
