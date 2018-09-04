a789dda3a480f39504c7dd23bbde2a3226a96e1f
Brian <brian.bacon01@gmail.com>
2018-09-03 22:14:55 -0400

    added new project

---
+++ Firmware/ADCTrainLocator/ADCTrainLocator/ADCTrainLocator/Sketch.cpp
@@ -0,0 +1,27 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+/*End of auto generated code by Atmel studio */
+
+
+//Beginning of Auto generated function prototypes by Atmel Studio
+//End of Auto generated function prototypes by Atmel Studio
+
+class Test
+{
+	
+public: 
+char* g = "test";
+};
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+int i = 3;
+int c = i + 5;
+}
